a67088b5e4f86c883d1f0f31371471ad26607d22
Piotr Rudzki <rudzky@air-piotr.home>
2023-09-01 12:13:01 +0200

    Improve code by using iterators

--- src/lib.rs
+++ src/lib.rs
@@ -7,23 +7,26 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments!");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
 
-        let ignore_case = if args.len() == 4 {
-            let flag = args[3].clone();
-            match flag.as_str() {
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
+        let ignore_case = match args.next() {
+            Some(flag) => match flag.as_str() {
                 "-i" | "--insensitive" => true,
                 "-s" | "--sensitive" => false,
                 _ => env::var("IGNORE_CASE").is_ok(),
-            }
-        } else {
-            env::var("IGNORE_CASE").is_ok()
+            },
+            None => env::var("IGNORE_CASE").is_ok(),
         };
 
         Ok(Self {
@@ -51,28 +54,17 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
+        .collect()
 }
 
 #[cfg(test)]
